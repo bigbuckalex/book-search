6784d9521272d1896cb28bebaf7911eb369cc05e
bigbuckalex <bigbuckalex@gmail.com>
2019-04-30 23:24:19 -0400

    added backend code, started front end

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import Nav from "./components/nav";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Nav />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ client/src/components/nav.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Nav = () => {
+    return (
+        <nav className="navbar navbar-dark">
+            <a href="/" className="navbar-brand">Books</a>
+        </nav>
+    );
+}
+
+export default Nav;
